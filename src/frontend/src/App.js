b6fe325630a752866f5a4f97237bd4b5d1c3f4b4
Tom Robinson <tom_robinson@rapid7.com>
2023-08-10 12:03:59 +0100

    10 aug

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 import {getAllStudents} from "./client";
+import {useState, useEffect} from "react";
 
 function App() {
+    const [students, setStudents] = useState([]);
 
-    getAllStudents()
-        .then(res => res.json())
-        .then(console.log)
-    return "hellooooo";
+    const fetchStudents = () => {
+        return getAllStudents()
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setStudents(data);
+            })
+    }
+
+    useEffect(() => {
+        console.log("component is mounted");
+        fetchStudents();
+    }, []);
+
+    if (students.length <= 0) {
+        return "No data";
+    }
+
+    return students.map((student, index) => {
+        return <p key={index}>{student.id} {student.name}</p>;
+    });
 }
 
 export default App;
