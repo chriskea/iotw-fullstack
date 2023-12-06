e0e76b4cb61f86cbdd98654809c01e57e70fa1dd
Tom Robinson <tom_robinson@rapid7.com>
2023-12-06 11:02:04 +0000

    6 dec

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,5 +1,13 @@
 package com.example.demo.student;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import lombok.*;
 
 @ToString
@@ -8,9 +16,28 @@ import lombok.*;
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Student {
+
+    @Id
+    @SequenceGenerator(name = "student_sequence",
+                       sequenceName = "student_sequence",
+                       allocationSize = 1)
+    @GeneratedValue(
+       generator = "student_sequence",
+       strategy = GenerationType.SEQUENCE)
+
     private Long id;
     private String name;
     private String email;
+
+    @Enumerated(EnumType.STRING)
     private Gender gender;
+
+    public Student(final String name, final String email, final Gender gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
 }
